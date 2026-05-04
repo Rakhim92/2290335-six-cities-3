c35f07b3a7cd925f8ec9f026e973ac2599078837
Rakhim <pharmamystery19@gmail.com>
2026-05-04 11:44:13 +0700

    Исправил логику в FavoritePage

--- src/pages/favorite-page/favorite-page.tsx
+++ src/pages/favorite-page/favorite-page.tsx
@@ -1,40 +1,47 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import PlaceCardsList from '../../components/place-card/place-cards-list';
 import {TOffer} from '../../types';
 import {useAppSelector} from '../../hooks';
-// import { api } from '../../store';
-import { MY_CITIES } from '../../const';
+
+type GroupedOffers = Record<string, TOffer[]>;
 
 const FavoritePage = (): JSX.Element => {
   const favorites = useAppSelector((state) => state.favorites);
   const [, setActiveOffer] = useState<TOffer>();
-  // const [favoriteOffers, setFavoriteOffers] = useState<TOffer[]>([]);
-  console.log(favorites);
 
+  const groupedByCity = favorites.reduce<GroupedOffers>((acc, offer) => {
+    const cityName = offer.city.name;
+
+    // Если города еще нет в объекте, создаем пустой массив
+    if (!acc[cityName]) {
+      acc[cityName] = [];
+    }
+
+    acc[cityName].push(offer);
+    return acc;
+  }, {});
 
   const handleHover = (offer?: TOffer) => {
     setActiveOffer(offer);
   };
 
-
-  // console.log(favoriteOffers);
   return (
     <main className="page__main page__main--favorites">
       <div className="page__favorites-container container">
         <section className="favorites">
           <h1 className="favorites__title">Saved listing</h1>
           <ul className="favorites__list">
-            {favorites && MY_CITIES.map((city) => (
-              <li className="favorites__locations-items" key={city.name}>
+            {Object.entries(groupedByCity).map(([cityName, cityOffers]) => (
+              <li className="favorites__locations-items" key={cityName}>
                 <div className="favorites__locations locations locations--current">
                   <div className="locations__item">
                     <a className="locations__item-link" href="#">
-                      <span>{city.name}</span>
+                      <span>{cityName}</span>
                     </a>
                   </div>
                 </div>
                 <PlaceCardsList
-                  offers={favorites}
+                  offers={cityOffers}
                   type={'favorites'}
                   handleHover = {handleHover}
                 />
